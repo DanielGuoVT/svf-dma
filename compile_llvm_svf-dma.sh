52073f797fb8d5c6cfabec69b65dc070a2207123
Shengjian Guo <guosj@vt.edu>
2019-11-15 14:24:29 +0800

    sjguo: tweak the compile_llvm_svf-dma.sh script.

--- compile_llvm_svf-dma.sh
+++ compile_llvm_svf-dma.sh
@@ -20,13 +20,13 @@ if [ $CNT_CPU -gt 2 ]; then
 fi
 
 # Install required packages
-#apt-get update -y --fix-missing
-#apt install -y wget subversion cmake libboost-dev libboost-system-dev git python subversion build-essential curl libcap-dev libncurses5-dev python-minimal python-pip unzip libtcmalloc-minimal4 libgoogle-perftools-dev libgmp-dev zlib1g-dev 
+apt-get update -y --fix-missing
+apt install -y wget subversion cmake libboost-dev libboost-system-dev git python subversion build-essential curl libcap-dev libncurses5-dev python-minimal python-pip unzip libtcmalloc-minimal4 libgoogle-perftools-dev libgmp-dev zlib1g-dev 
 
 
 # Download & build llvm 6.0 and clang 6.0 
 llvm_version=$(llvm-config --version)
-if [ "$llvm_version" != "6.0.0" ]; then
+#if [ "$llvm_version" != "6.0.0" ]; then
     # URLs
     URL_LLVM=http://releases.llvm.org/6.0.0/llvm-6.0.0.src.tar.xz
     URL_CLANG=http://releases.llvm.org/6.0.0/cfe-6.0.0.src.tar.xz
@@ -43,7 +43,7 @@ if [ "$llvm_version" != "6.0.0" ]; then
 		export LLVM_SRC=./llvm
 		export LLVM_DIR=./llvm/build
 		export PATH=$LLVM_DIR/bin:$PATH
-fi
+#fi
 
 # Build SVF with our dma library
 DIRECTORY=Release-build
@@ -53,5 +53,7 @@ fi
 
 cd $DIRECTORY
 cmake ../
-make -j
+make -j $MAKE_JOBS
+
+
 
