43914fdceb38b5c47e003e4921f75bc7bc7336aa
Shengjian Guo <guosj@vt.edu>
2019-11-18 07:41:26 -0500

    sjguo: revise the compilation script and test it on a clean ubuntu 18.04.

--- compile_llvm_svf-dma.sh
+++ compile_llvm_svf-dma.sh
@@ -16,13 +16,13 @@ function download() {
 MAKE_JOBS=1
 CNT_CPU=$(grep -c ^processor /proc/cpuinfo)
 if [ $CNT_CPU -gt 2 ]; then
-	MAKE_JOBS=$((CNT_CPU / 2))
+	MAKE_JOBS=$((CNT_CPU - 1))
 fi
 
-# Install required packages
-apt-get update -y --fix-missing
-apt install -y wget subversion cmake libboost-dev libboost-system-dev git python subversion build-essential curl libcap-dev libncurses5-dev python-minimal python-pip unzip libtcmalloc-minimal4 libgoogle-perftools-dev libgmp-dev zlib1g-dev 
 
+# Install required packages
+# apt-get update -y --fix-missing
+# apt install -y wget subversion cmake libboost-dev libboost-system-dev git python subversion build-essential curl libcap-dev libncurses5-dev python-minimal python-pip unzip libtcmalloc-minimal4 libgoogle-perftools-dev libgmp-dev zlib1g-dev 
 
 # Download & build llvm 6.0 and clang 6.0 
 llvm_version=$(llvm-config --version)
@@ -31,29 +31,33 @@ llvm_version=$(llvm-config --version)
     URL_LLVM=http://releases.llvm.org/6.0.0/llvm-6.0.0.src.tar.xz
     URL_CLANG=http://releases.llvm.org/6.0.0/cfe-6.0.0.src.tar.xz
 
+    rm -fr llvm
     download $URL_LLVM llvm 
     download $URL_CLANG llvm/tools/clang
 
     mkdir -p llvm/build
-    cd llvm/build
-    cmake -DLLVM_ENABLE_RTTI=ON -DLLVM_BUILD_RUNTIME=ON -DLLVM_ENABLE_ZLIB=ON -DCMAKE_INSTALL_PREFIX=/usr ..
+
+    export LLVM_SRC=$(pwd)/llvm
+    export LLVM_DIR=$(pwd)/llvm/build
+    export PATH=$LLVM_DIR/bin:$PATH
+
+    cd $LLVM_DIR 
+
+    cmake -DCMAKE_BUILD_TYPE=MinSizeRel -DLLVM_BUILD_RUNTIME=ON -DCMAKE_INSTALL_PREFIX=/usr ..
     make -j$MAKE_JOBS
 
-		cd ../..
-		export LLVM_SRC=./llvm
-		export LLVM_DIR=./llvm/build
-		export PATH=$LLVM_DIR/bin:$PATH
+    cd ../..
 #fi
 
 # Build SVF with our dma library
 DIRECTORY=Release-build
 if [ ! -d "$DIRECTORY" ]; then
-	mkdir $DIRECTORY
+    mkdir $DIRECTORY
 fi
 
 cd $DIRECTORY
 cmake ../
-make -j $MAKE_JOBS
+make -j$MAKE_JOBS
 
 
 
